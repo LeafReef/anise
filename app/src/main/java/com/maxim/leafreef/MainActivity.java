d891f6d078addc7182182e1e981c50e6c2f6a5ec
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-05-19 21:38:04 +0300

    Refactor names, remove assets and change color theme

--- app/src/main/java/com/maxim/leafreef/MainActivity.java
+++ app/src/main/java/com/maxim/leafreef/MainActivity.java
@@ -3,26 +3,20 @@ package com.maxim.leafreef;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.VideoView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,69 +37,28 @@ public class MainActivity extends AppCompatActivity {
     public void getSensorData() {
         String url = "https://leafreef.herokuapp.com/api/list";
 
-        TextView tempText = (TextView) findViewById(R.id.temperature_text);
-        TextView humText = (TextView) findViewById(R.id.humidity_text);
+        TextView tempText = findViewById(R.id.temperature_text);
+        TextView humText = findViewById(R.id.humidity_text);
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            temperature = response.getInt("temperature");
-                            humidity = response.getInt("humidity");
+        @SuppressLint("SetTextI18n") JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        temperature = response.getInt("temperature");
+                        humidity = response.getInt("humidity");
 
-                            System.out.println(temperature);
-
-                            tempText.setText(temperature + "°C");
-                            humText.setText(humidity + "%");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        tempText.setText(temperature + "°C");
+                        humText.setText(humidity + "%");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError e) {
-                System.out.println("Error getting data");
-                e.printStackTrace();
-            }
-        });
+                }, Throwable::printStackTrace);
 
         queue.add(request);
     }
 
-    public void sendPostRequest(String url) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        System.out.println(response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
-        queue.add(stringRequest);
-    }
-
     public void refreshSensorData(View view) {
         getSensorData();
-    }
-
-    public void toggleOn(View view) {
-        String onUrl = "http://192.168.1.8:3000/on";
-
-        sendPostRequest(onUrl);
-    }
-
-    public void toggleOff(View view) {
-        String onUrl = "http://192.168.1.8:3000/off";
-
-        sendPostRequest(onUrl);
     }
 }
